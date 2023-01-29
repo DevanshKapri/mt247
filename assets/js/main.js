2d4d3703f0f31d16a382497a6f138cc7a7903dc3
Devansh Kapri <devanshkapri19@gmail.com>
2023-01-29 22:12:28 +0530

    responsiveness fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,3 +117,12 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   });
 });
+
+const btn = document.getElementById("menu-btn");
+const nav = document.getElementById("menu");
+
+btn.addEventListener("click", () => {
+  btn.classList.toggle("open");
+  nav.classList.toggle("flex");
+  nav.classList.toggle("hidden");
+});
